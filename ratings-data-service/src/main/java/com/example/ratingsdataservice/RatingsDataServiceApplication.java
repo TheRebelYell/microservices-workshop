687aba6b889072a0e4d6fe5a4a5d51deff6c0bd9
TheRebelYell <dark777sovereign@gmail.com>
2019-07-22 02:14:34 +0300

    init services

---
+++ ratings-data-service/src/main/java/com/example/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsDataServiceApplication.class, args);
+    }
+}
